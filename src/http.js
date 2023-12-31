637438c060e45a7ba5382d5cf04f6550ba649614
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-12-31 11:05:40 +0300

    sending data with POST

--- src/http.js
+++ src/http.js
@@ -8,3 +8,18 @@ export async function fetchAvailablePlaces() {
 
     return resData.places
 }
+
+export async function updateUserPlaces(places) {
+    const response = await fetch("http://localhost:3000/user-places", {
+        method: "PUT",
+        body: JSON.stringify({ places }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+
+    if (!response.ok) {
+        throw new Error("Updating place failed")
+    }
+    return response.message
+}
